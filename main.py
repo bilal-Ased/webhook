31db6edff8acb1e57b75c5fff1e059ef6e345616
Bilal Mughal <bilalmughal@Bilals-MacBook-Air.local>
2025-11-24 20:10:37 +0300

    worked on incoming call endpoint

--- main.py
+++ main.py
@@ -5,3 +5,9 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"message": "Welcome"}
+
+
+@app.get("/incoming-call")
+def incoming_call(call_id: str, caller_name: str):
+    print(call_id, caller_name)
+    return {"status": "ok"}
